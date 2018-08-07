8201d175cd2fcdf6dd87a4982116af72f5182680
Echno126 <15620742723@163.com>
2018-08-07 17:26:26 +0800

    1.添加tomcat依赖，同时配置springboot可以使用外部容器进行启动;

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,12 +3,24 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //return super.configure(builder);
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    //TODO SpringBootServletInitializer 集成该方法可以使用外部tomcat进行启动项目，其起到web.xml 文件的作用，
+    //TODO 源码分析地址: https://blog.csdn.net/songhaifengshuaige/article/details/54138023
 }
